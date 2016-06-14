9663868904d9458199b44d96f63bb2d902a9f65d
Ahmed Aly <ahmaly01@gmail.com>
2016-06-14 16:30:03 -0400

    added a connection class to detect if you are connected to the internet

---
+++ app/src/main/java/com/example/aaly/rettiwt/data/remote/ConnectionDetector.java
@@ -0,0 +1,26 @@
+package com.example.aaly.rettiwt.data.remote;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectionDetector {
+    private Context context;
+
+    public ConnectionDetector(Context context) {
+        this.context = context;
+    }
+
+    public boolean isConnectingToInternet() {
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null) {
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
